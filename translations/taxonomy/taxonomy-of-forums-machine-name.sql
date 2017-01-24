f4fe87dad5736f3367508f75799cb74200651e62
Märt Põder <boamaod@gmail.com>
2017-01-24 11:01:18 +0200

    SQL to create machine_name fields for forum taxonomy.

---
+++ translations/taxonomy/taxonomy-of-forums-machine-name.sql
@@ -0,0 +1,5 @@
+INSERT INTO field_data_field_machine_name SELECT 'taxonomy_term', 'forums', 0, tid, tid, 'und', 0, 'avaandmetest-laiemalt', NULL FROM taxonomy_term_data WHERE name='Open data';
+INSERT INTO field_data_field_machine_name SELECT 'taxonomy_term', 'forums', 0, tid, tid, 'und', 0, 'üldine-diskussioon', NULL FROM taxonomy_term_data WHERE name='General discussion';
+INSERT INTO field_data_field_machine_name SELECT 'taxonomy_term', 'forums', 0, tid, tid, 'und', 0, 'töövahendid-ja-rakendused', NULL FROM taxonomy_term_data WHERE name='Tools and applications';
+INSERT INTO field_data_field_machine_name SELECT 'taxonomy_term', 'forums', 0, tid, tid, 'und', 0, 'andmete-kasutamine', NULL FROM taxonomy_term_data WHERE name='Usage of data';
+INSERT INTO field_data_field_machine_name SELECT 'taxonomy_term', 'forums', 0, tid, tid, 'und', 0, 'andmete-publitseerimine', NULL FROM taxonomy_term_data WHERE name='Data publishing';
